f35a9505aff5a64d470b97bd3b5db09e23a42d2a
shajada13 <shajadamasum511@gmail.com>
2025-07-28 01:48:50 +0600

    added loops

---
+++ Loop/forLoop.js
@@ -0,0 +1,15 @@
+
+
+for (let i = 1 ; i <=5 ; i++){ // initialize i , (i++) means = increase ta value ; (i <=5 ) it means i always less then 5 ; it works 5 time ;
+    console.log("Thanks to you "); 
+}
+
+// sum of value 
+
+sum = 0 ; // if you sum something , you must initialize its to 0 ; 
+for (let i = 1 ; i <= 10 ; i++){
+    sum += i ;
+}
+
+console.log ("Total sum = ", sum);
+
---
+++ Loop/infinity.js
@@ -0,0 +1,7 @@
+
+// its allocate all the memory , and your system collapse , 
+// don't try in your laptop 
+
+for ( let i = 0 ; i >= 0 ; i++){
+    console.log ( " ha ha ha " , i );
+}
